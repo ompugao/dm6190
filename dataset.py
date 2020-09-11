66c13d8cd3836f18a7e274e91adbc591287ba34a
Shohei Fujii <fujii.shohei@gmail.com>
2020-09-11 12:55:41 +0900

    update dataset to resize image automatically

--- dataset.py
+++ dataset.py
@@ -25,7 +25,7 @@ def imshow(imgs, masks):
     plt.show()
 
 class Dataset(VisionDataset):
-    def __init__(self, root, train=None, small=False, transforms=None, transform=None, target_transform=None):
+    def __init__(self, root, train=None, imgsize=None, transforms=None, transform=None, target_transform=None):
         super(Dataset, self).__init__(root, transforms=transforms, transform=transform, target_transform=target_transform)
         if train:
             csvfile = 'train.csv'
@@ -35,7 +35,7 @@ class Dataset(VisionDataset):
         df = pd.read_csv(abscsvpath, header=None)
         self.filenames = ['%03d'%d for d in df[0].values]
 
-        self.small = small
+        self.imgsize = imgsize
 
     def __len__(self):
         return len(self.filenames)
@@ -44,14 +44,40 @@ class Dataset(VisionDataset):
         root = pathlib.Path(self.root).resolve()
         filename = self.filenames[index]
         imgdir = 'original_images'
-        maskdir = 'label_images_semantic' 
-        if self.small:
-            imgdir = 'small_images'
-            maskdir = 'small_label_images_semantic' 
+        maskdir = 'label_images_semantic'
         imgfile = root / imgdir  / (filename +'.jpg')
         maskfile  = root / maskdir / (filename +'.png')
-        img = np.array(default_loader(imgfile))
-        mask = np.array(default_loader(maskfile).convert('L')) #convert to grayscale again
+
+        resizedimgdir = 'resized_images'
+        resizedmaskdir = 'resized_label_images_semantic'
+
+        img = None
+        mask = None
+        if self.imgsize is not None:
+            suffix = '_' + str(self.imgsize[0]) + 'x' + str(self.imgsize[1])
+            smallimgfile = root / resizedimgdir  / (filename + suffix + '.jpg')
+            smallmaskfile  = root / resizedmaskdir / (filename + suffix + '.png')
+            if smallimgfile.exists() and smallmaskfile.exists():
+                img = np.array(default_loader(smallimgfile))
+                mask = np.array(default_loader(smallmaskfile).convert('L')) #convert to grayscale again
+            else:
+                if not (root / resizedimgdir).exists():
+                    (root / resizedimgdir).mkdir(parents=True)
+                if not (root / resizedmaskdir).exists():
+                    (root / resizedmaskdir).mkdir(parents=True)
+                img = default_loader(imgfile)
+                mask = default_loader(maskfile).convert('L') #convert to grayscale again
+
+                #resize and cache to disk
+                img = img.resize(self.imgsize)
+                mask = mask.resize(self.imgsize)
+                img.save(smallimgfile)
+                mask.save(smallmaskfile)
+                img = np.array(img)
+                mask = np.array(mask)
+        else:
+            img = np.array(default_loader(imgfiletoload))
+            mask = np.array(default_loader(maskfiletoload).convert('L')) #convert to grayscale again
         return self.transforms(image=img, mask=mask)
 
 if __name__ == '__main__':
