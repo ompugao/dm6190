45f91c348bf4b0b1176a6e3618be39e58279eebe
Shohei Fujii <fujii.shohei@gmail.com>
2020-11-18 16:17:40 +0900

    remove useless lines

--- checkmodels.sh
+++ checkmodels.sh
@@ -11,18 +11,6 @@ python test.py -model=deeplabv3plus -logdir='logs/deeplabv3plus_unfreeze_decoder
 python test.py -model=pan -logdir='logs/pan_freeze_decoder_2020-09-16 09-52-11.379602' -augmentation_version=0 -imgheight=320 -imgwidth=480
 python test.py -model=pan -logdir='logs/pan_unfreeze_decoder_2020-09-16 10-33-12.988805' -augmentation_version=0 -imgheight=320 -imgwidth=480
 
-# Mean IoU: 0.205153, Pixel Accuracy Sum: 67.187676, mean: 0.839846
-# Mean IoU: 0.187193, Pixel Accuracy Sum: 66.241602, mean: 0.828020
-# Mean IoU: 0.215810, Pixel Accuracy Sum: 67.717376, mean: 0.846467
-# Mean IoU: 0.213285, Pixel Accuracy Sum: 67.560085, mean: 0.844501
-# Mean IoU: 0.221303, Pixel Accuracy Sum: 68.337969, mean: 0.854225
-# Mean IoU: 0.217960, Pixel Accuracy Sum: 68.064935, mean: 0.850812
-# Mean IoU: 0.186710, Pixel Accuracy Sum: 65.891452, mean: 0.823643
-# Mean IoU: 0.200851, Pixel Accuracy Sum: 66.833034, mean: 0.835413
-# Mean IoU: 0.163800, Pixel Accuracy Sum: 63.735495, mean: 0.796694
-# Mean IoU: 0.186507, Pixel Accuracy Sum: 64.505397, mean: 0.806317
-
-
 python test.py -model=unet -logdir='logs/unet_freeze_decoder_2020-09-16 11-14-03.715160' -augmentation_version=0 -imgheight=640 -imgwidth=960
 python test.py -model=unet -logdir='logs/unet_unfreeze_decoder_2020-09-16 12-27-43.885046' -augmentation_version=0 -imgheight=640 -imgwidth=960
 python test.py -model=fpn -logdir='logs/fpn_freeze_decoder_2020-09-16 13-41-20.060483' -augmentation_version=0 -imgheight=640 -imgwidth=960
@@ -30,31 +18,4 @@ python test.py -model=fpn -logdir='logs/fpn_unfreeze_decoder_2020-09-16 14-43-17
 python test.py -model=pan -logdir='logs/pan_freeze_decoder_2020-09-17 11-27-21.809318' -augmentation_version=0 -imgheight=640 -imgwidth=960
 python test.py -model=pan -logdir='logs/pan_unfreeze_decoder_2020-09-17 11-28-05.393323' -augmentation_version=0 -imgheight=640 -imgwidth=960
 
-
-# Mean IoU: 0.243431, Pixel Accuracy Sum: 69.757715, mean: 0.871971
-# Mean IoU: 0.255911, Pixel Accuracy Sum: 70.520202, mean: 0.881503
-# Mean IoU: 0.268006, Pixel Accuracy Sum: 71.170475, mean: 0.889631
-# Mean IoU: 0.253208, Pixel Accuracy Sum: 70.479209, mean: 0.880990
-# Mean IoU: 0.240053, Pixel Accuracy Sum: 69.698047, mean: 0.871226
-# Mean IoU: 0.195554, Pixel Accuracy Sum: 60.221577, mean: 0.752770
-
-
-
-leus@hpcs:~/3rdparty/github.com/ompugao/dm6190/1_segmentationreview$ bash checkmodels.sh
-Mean IoU: 0.417500, Pixel Accuracy Sum: 67.187676, mean: 0.839846
-Mean IoU: 0.366250, Pixel Accuracy Sum: 66.241602, mean: 0.828020
-Mean IoU: 0.451250, Pixel Accuracy Sum: 67.717376, mean: 0.846467
-Mean IoU: 0.433750, Pixel Accuracy Sum: 67.560085, mean: 0.844501
-Mean IoU: 0.485000, Pixel Accuracy Sum: 68.337969, mean: 0.854225
-Mean IoU: 0.460000, Pixel Accuracy Sum: 68.064935, mean: 0.850812
-Mean IoU: 0.368750, Pixel Accuracy Sum: 65.891452, mean: 0.823643
-Mean IoU: 0.406250, Pixel Accuracy Sum: 66.833034, mean: 0.835413
-Mean IoU: 0.276250, Pixel Accuracy Sum: 63.735495, mean: 0.796694
-Mean IoU: 0.343750, Pixel Accuracy Sum: 64.505397, mean: 0.806317
-Mean IoU: 0.523750, Pixel Accuracy Sum: 69.757715, mean: 0.871971
-Mean IoU: 0.561250, Pixel Accuracy Sum: 70.520202, mean: 0.881503
-Mean IoU: 0.576250, Pixel Accuracy Sum: 71.170475, mean: 0.889631
-Mean IoU: 0.550000, Pixel Accuracy Sum: 70.479209, mean: 0.880990
-Mean IoU: 0.497500, Pixel Accuracy Sum: 69.698047, mean: 0.871226
-Mean IoU: 0.387500, Pixel Accuracy Sum: 60.221577, mean: 0.752770
 
--- checkmodels2.sh
+++ checkmodels2.sh
@@ -20,26 +20,3 @@ echo python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_versio
 python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir='logs/pan_freeze_decoder_aug1_480_320_2020-09-20 07-26-47.115591/' -gpu=0
 echo python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir='logs/pan_unfreeze_decoder_aug1_480_320_2020-09-20 13-22-00.710588/' -gpu=0
 python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir='logs/pan_unfreeze_decoder_aug1_480_320_2020-09-20 13-22-00.710588/' -gpu=0
-
-
-# leus@hpcs:~/3rdparty/github.com/ompugao/dm6190/1_segmentationreview$ bash checkmodels2.sh
-# python test.py -model=unet -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/unet_freeze_decoder_aug1_480_320_2020-09-17 15-03-34.890921/ -gpu=0
-# Mean IoU: 0.322500, Pixel Accuracy Sum: 64.557721, mean: 0.806972
-# python test.py -model=unet -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/unet_unfreeze_decoder_aug1_480_320_2020-09-17 21-14-48.742430/ -gpu=0
-# Mean IoU: 0.350000, Pixel Accuracy Sum: 65.690228, mean: 0.821128
-# python test.py -model=fpn -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/fpn_freeze_decoder_aug1_480_320_2020-09-18 03-22-41.124707/ -gpu=0
-# Mean IoU: 0.492500, Pixel Accuracy Sum: 69.742057, mean: 0.871776
-# python test.py -model=fpn -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/fpn_unfreeze_decoder_aug1_480_320_2020-09-18 09-38-52.856183/ -gpu=0
-# Mean IoU: 0.437500, Pixel Accuracy Sum: 68.175736, mean: 0.852197
-# python test.py -model=deeplabv3 -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/deeplabv3_freeze_decoder_aug1_480_320_2020-09-18 15-54-46.205066/ -gpu=0
-# Mean IoU: 0.472500, Pixel Accuracy Sum: 67.705671, mean: 0.846321
-# python test.py -model=deeplabv3 -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/deeplabv3_unfreeze_decoder_aug1_480_320_2020-09-19 05-15-37.396124/ -gpu=0
-# Mean IoU: 0.525000, Pixel Accuracy Sum: 70.484258, mean: 0.881053
-# python test.py -model=deeplabv3plus -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/deeplabv3plus_freeze_decoder_aug1_480_320_2020-09-19 18-36-23.957823/ -gpu=0
-# Mean IoU: 0.461250, Pixel Accuracy Sum: 68.122207, mean: 0.851528
-# python test.py -model=deeplabv3plus -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/deeplabv3plus_unfreeze_decoder_aug1_480_320_2020-09-20 01-07-17.577715/ -gpu=0
-# Mean IoU: 0.491250, Pixel Accuracy Sum: 69.540378, mean: 0.869255
-# python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/pan_freeze_decoder_aug1_480_320_2020-09-20 07-26-47.115591/ -gpu=0
-# Mean IoU: 0.240000, Pixel Accuracy Sum: 58.732096, mean: 0.734151
-# python test.py -model=pan -imgwidth=480 -imgheight=320 -augmentation_version=1 -logdir=logs/pan_unfreeze_decoder_aug1_480_320_2020-09-20 13-22-00.710588/ -gpu=0
-# Mean IoU: 0.250000, Pixel Accuracy Sum: 56.751686, mean: 0.709396
